7f674d85eda947accb4cdddf59f8b8cb304b83bb
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-24 20:37:17 +0530

    import the Order interface, test it by creating dummy variables that uses Order interface

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import type { Order } from "./interface.js";
+
+const order1: Order = {
+  userId : "1",
+  price : 100,
+  quantity : 1,
+  side : "Buy"
+}
+
+
+console.log(order1)
+
+
+
+const order2: Order = {
+  userId : "2",
+  price : 100,
+  quantity : 1,
+  side : "Sell"
+}
+
+
+console.log(order2)
