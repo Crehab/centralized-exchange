f887396f7e875265643e0b522fe1f9ec6857713f
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-24 23:28:32 +0530

    create OrderBook.ts file, import the Order interface, write the class for the state of the market.

---
+++ src/OrderBook.ts
@@ -0,0 +1,17 @@
+import type { Order } from "./interface.js";
+
+export class OrderBook {
+  bids: Order[] = [];
+  asks: Order[] = [];
+
+  // this is the method to process new Orders.
+  addOrder(newOrder: Order) {
+    if (newOrder.side === "Buy") {
+      this.bids.push(newOrder);
+      
+    } else {
+      this.asks.push(newOrder);
+    }
+  }
+}
+
