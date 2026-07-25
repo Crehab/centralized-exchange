dc62d0e2e7a0e4652edbf713e86575d9948a363e
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-25 14:36:39 +0530

    write the method of addOrder and Sorting code for bid and ask

--- src/OrderBook.ts
+++ src/OrderBook.ts
@@ -6,13 +6,19 @@ export class OrderBook {
 
   // this is the method to process new Orders.
   addOrder(newOrder: Order) {
-    if (newOrder.side === "Buy") {
+    if (newOrder.side === "buy") {
       this.bids.push(newOrder);
-      
+      // bids sorting is here
+      this.bids.sort((a, b): number => {
+       return  b.price - a.price; // Desending order for bids (Buy orders)
+        
+      })
     } else {
       this.asks.push(newOrder);
+      // ask soring is here
+      this.asks.sort((a, b): number  => {
+        return a.price - b.price;// Ascending order for asks (Sell orders)
+      })
     }
   }
 }
-
- 
