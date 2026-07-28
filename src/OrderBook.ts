2e8a54bc98f2f8bfabb6474f03dd6b39845563c5
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-28 17:36:56 +0530

    the first code for matching algo contains error in the OrderBook.ts

--- src/OrderBook.ts
+++ src/OrderBook.ts
@@ -26,16 +26,19 @@ export class OrderBook {
   // write the logic that connects a buyer and a seller.
 
     match() {
-      while(this.bids.length > 0 && this.asks.length > 0) {
-        if (this.bids.price >= this.asks.price) {
+      while (this.bids.length > 0 && this.asks.length > 0) {
+        
+        if (this.bids.price  >= this.asks.price) {
           Math.min(this.bids.quantity, this.asks.quantity);
           const subtract = this.bids.quantity - this.asks.quantity;
+
           if (this.bids.quantity === null) {
             this.bids.shift()
           } else if (this.asks.quantity === null) {
             this.asks.shift()
           }
-        } else
+          
+       } else
           break;
       }
     }
