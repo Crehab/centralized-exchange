708ac324681b169da46e547d632e57650a20a533
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-28 17:33:25 +0530

    write the Matching algorithm that connects buyer and seller.

--- src/OrderBook.ts
+++ src/OrderBook.ts
@@ -15,16 +15,30 @@ export class OrderBook {
       })
     } else {
       this.asks.push(newOrder);
-      // ask soring is here
+      // ask sorting is here
       this.asks.sort((a, b): number  => {
         return a.price - b.price;// Ascending order for asks (Sell orders)
       })
     }
+    this.match() // try to match immediately after adding
   }
   // TODO -  The Matching Algorithm 
   // write the logic that connects a buyer and a seller.
-  
-  
+
+    match() {
+      while(this.bids.length > 0 && this.asks.length > 0) {
+        if (this.bids.price >= this.asks.price) {
+          Math.min(this.bids.quantity, this.asks.quantity);
+          const subtract = this.bids.quantity - this.asks.quantity;
+          if (this.bids.quantity === null) {
+            this.bids.shift()
+          } else if (this.asks.quantity === null) {
+            this.asks.shift()
+          }
+        } else
+          break;
+      }
+    }
 
 
   
