5b009b7f569f0e967ab0cf5fb3ff816eeec5911e
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-31 14:56:40 +0530

    write the code that connects client to the server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 }); // create new server
+
+wss.on('connection', function connection(ws) {
+  console.log(`A new client connected!`);
+
+
+  
+}
