416a90bf9634ced046c258b99426d39f57d3ca90
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-31 15:01:47 +0530

    write the remaining code to create a ws server.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,14 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 }); // create new server
 
+// client connects to the server.
 wss.on('connection', function connection(ws) {
-  console.log(`A new client connected!`);
+  console.log('A new client connected!');
 
-
-  
-}
+  // do this when THIS specific client sends message.
+  ws.on('message', function message(data) {
+    console.log('Received raw data from client:', data.toString());
+    // send a message back to the client.
+    ws.send('Message received by the exchange server.')
+  })
+})
