6ce592d470a6e6c4e712fa84db2e4be20358b5c9
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-07-31 15:36:33 +0530

    update the ws code.

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,20 @@ wss.on('connection', function connection(ws) {
 
   // do this when THIS specific client sends message.
   ws.on('message', function message(data) {
-    console.log('Received raw data from client:', data.toString());
-    // send a message back to the client.
-    ws.send('Message received by the exchange server.')
+    // convert raw data string to JSON format. 
+    const messageString = data.toString();
+    const parsedMessage = JSON.parse(messageString);
+
+    // CREATE ORDER ACTION 
+    if (parsedMessage === 'CREATE_ORDER') {
+      console.log("Client wants to create an order!");
+      console.log("Order details:", parsedMessage.data);
+
+      // send A JSON object back confirming order receipt.
+      ws.send(JSON.stringify({ status: "success", message: "Order received" }));
+    } else {
+      console.log("Unknown action received.");
+    }
   });
 });
 
