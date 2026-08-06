4a6ae266ddbde0d1c07a8757f973e88dbf9fa4a2
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-08-06 16:01:05 +0530

    create new order book instance and place it before the web socket server creation

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,51 @@
-import { error } from 'console';
-import { WebSocketServer } from 'ws';
-import {OrderBook} from './orderBook.js';
+import { WebSocketServer } from "ws";
+import { OrderBook } from "./orderBook.js";
+
+// create the orderbook instance for this specific client connection.
+const orderBook = new OrderBook();
+
 const wss = new WebSocketServer({ port: 8080 }); // create new server
 
 // client connects to the server.
-wss.on('connection', function connection(ws) {
-    console.log('A new client connected!');
-    
+wss.on("connection", function connection(ws) {
+    console.log("A new client connected!");
 
-  // do this when THIS specific client sends message.
-    ws.on('message', function message(data) {
+    // do this when THIS specific client sends message.
+    ws.on("message", function message(data) {
         // convert raw data string to JSON format.
         const messageString = data.toString();
 
         try {
             const parsedMessage = JSON.parse(messageString);
             // CREATE ORDER ACTION
-            if (parsedMessage.action === 'CREATE_ORDER') {
+            if (parsedMessage.action === "CREATE_ORDER") {
                 console.log("Client wants to create an order!");
                 console.log("Order details:", parsedMessage.data);
 
+                // capture the data field in a new variable called order.
+                const order = parsedMessage.data;
+                // pass it to the orderBook's addOrder method to process it using match().
+                orderBook.addOrder(order);
+                console.log("Bids:", orderBook.bids);
+                console.log("Asks:", orderBook.asks);
                 // send A JSON object back confirming order receipt.
-                ws.send(JSON.stringify({ status: "success", message: "Order received" }));
+                ws.send(
+                    JSON.stringify({
+                        status: "success",
+                        message: "Order has been placed and processed.",
+                    }),
+                );
             } else {
-                console.log("Unknown action received.");
+                console.log("Order could not be placed.");
             }
-        }
-        catch (error) {
+        } catch (error) {
             console.error("Error parsing message:", error);
-            ws.send(JSON.stringify({ status: "error", message: "Invalid JSON in message" }));
+            ws.send(
+                JSON.stringify({
+                    status: "error",
+                    message: "Invalid JSON in message",
+                }),
+            );
         }
     });
 });
