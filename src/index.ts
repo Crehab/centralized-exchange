240776a62e144332a9f59a59a676e2d5e2f4cf37
Rohan Badiger <rohanbadiger4738@gmail.com>
2026-08-24 16:18:23 +0530

    indentation update in index file and remove logs for order1 and order2

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,22 @@ import type { Order } from "./interface.js";
 import { OrderBook } from "./orderBook.js";
 
 const order1: Order = {
-  userId: "1",
-  price : 100,
-  quantity : 1,
-  side : "buy"
+    userId: "1",
+    price: 100,
+    quantity: 1,
+    side: "buy"
 }
-// console.log(order1)
 const order2: Order = {
-  userId : "2",
-  price : 100,
-  quantity : 1,
-  side : "sell"
+    userId: "2",
+    price: 100,
+    quantity: 1,
+    side: "sell"
 }
-// console.log(order2)
 
 const myBook = new OrderBook();
 myBook.addOrder({ userId: "1", side: "buy", price: 100, quantity: 2 });
 myBook.addOrder({ userId: "2", side: "buy", price: 105, quantity: 5 });
-myBook.addOrder({ userId: "3", side : "sell", price: 110, quantity: 1 });
+myBook.addOrder({ userId: "3", side: "sell", price: 110, quantity: 1 });
 myBook.addOrder({ userId: "4", side: "sell", price: 108, quantity: 3 });
 
 console.log("BIDS (Buyers):", myBook.bids);
